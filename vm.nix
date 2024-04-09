b982955fbea87323432673b5031d730e8a7c6ba8
IogaMaster <iogamastercode@gmail.com>
2024-04-09 14:39:23 -0600

    feat(vm): remove

--- vm.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  imports = [./modules/nixos/dynamo/default.nix];
-
-  dynamo.servers.minecraft."baller" = {
-    enable = true;
-    manifest = null;
-    reverseProxy = false;
-  };
-}
