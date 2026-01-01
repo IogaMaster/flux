3aacffc6bd255e8ce835ccc61dd2b11f5e5fc5d8
IogaMaster <iogamastercode@gmail.com>
2025-12-31 18:04:30 -0700

    fix: overlay output
    test

--- builders/mkMinecraftServer.nix
+++ builders/mkMinecraftServer.nix
@@ -1,44 +1,21 @@
-{
-  lib,
-  mkGenericServer,
-  mcman,
-  jre,
-  jre8,
-  ...
-}: {
-  name ? "",
-  src ? null,
-  serverLocation ? null,
-  hash ? "",
-  meta ? {},
-  ...
-}:
+{ lib, mkGenericServer, mcman, jre, jre8, ... }:
+{ name ? "", src ? null, serverLocation ? null, hash ? "", meta ? { }, ... }:
 mkGenericServer {
   inherit name src serverLocation hash;
 
-  nativeBuildInputs = [
-    mcman
-    jre
-    jre8
-  ];
+  nativeBuildInputs = [ mcman jre jre8 ];
 
-  buildInputs = [
-    mcman
-    jre
-    jre8
-  ];
-  buildPhase =
-    ''
-      HOME=$TMPDIR
-      CI=true # Better build logs
-      cd $src
-    ''
-    + lib.optionalString (serverLocation != null) ''
-      cd ${serverLocation}
-    ''
-    + ''
-      mcman build -o $out
-    '';
+  buildInputs = [ mcman jre jre8 ];
+
+  buildPhase = ''
+    HOME=$TMPDIR
+    CI=true # Better build logs
+    cd $src
+  '' + lib.optionalString (serverLocation != null) ''
+    cd ${serverLocation}
+  '' + ''
+    mcman build -o $out
+  '';
 
   startCmd = "./start.sh";
 
--- flake.nix
+++ flake.nix
@@ -6,37 +6,36 @@
     mcman.url = "github:deniz-blue/mcman";
   };
 
-  outputs = {nixpkgs, ...}@inputs: let
-    inherit (nixpkgs) lib;
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-      ] (system: function nixpkgs.legacyPackages.${system});
-  in rec {
-    devShells = forAllSystems (pkgs: {
-      default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          alejandra
-        ];
+  outputs = { nixpkgs, ... }@inputs:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
+        (system: function nixpkgs.legacyPackages.${system});
+    in rec {
+      devShells = forAllSystems (pkgs: {
+        default =
+          pkgs.mkShell { nativeBuildInputs = with pkgs; [ alejandra ]; };
+      });
+
+      packages = lib.recursiveUpdate
+        (forAllSystems (pkgs: { playit = pkgs.callPackage ./pkgs/playit { }; }))
+        inputs.mcman.packages;
+
+      nixosModules.flux = ./modules/flux;
+      nixosModules.default = nixosModules.flux;
+
+      overlays.default = final: prev: {
+        inherit (inputs.mcman.packages.${final.stdenv.hostPlatform.system})
+          mcman;
+        playit = final.callPackage ./pkgs/playit { };
+
+        mkGenericServer = final.callPackage ./builders/mkGenericServer.nix { };
+        mkMinecraftServer =
+          final.callPackage ./builders/mkMinecraftServer.nix { };
+        mkSteamServer = final.callPackage ./builders/mkSteamServer.nix { };
+
+        fetchSteam = final.callPackage ./helpers/fetchSteam { };
       };
-    });
-
-    packages = lib.recursiveUpdate (forAllSystems (pkgs: {
-      playit = pkgs.callPackage ./pkgs/playit {};
-    })) inputs.mcman.packages;
-
-    nixosModules.flux = ./modules/flux;
-    nixosModules.default = nixosModules.flux;
-
-    overlays = (final: prev: {
-      playit = final.callPackage ./pkgs/playit {};
-
-      mkGenericServer = final.callPackage ./builders/mkGenericServer.nix {};
-      mkMinecraftServer = final.callPackage ./builders/mkMinecraftServer.nix {};
-      mkSteamServer = final.callPackage ./builders/mkSteamServer.nix {};
-
-      fetchSteam = final.callPackage ./helpers/fetchSteam {};
-    } // inputs.mcman.overlays.default);
-  };
+    };
 }
